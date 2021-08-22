e604f15a87de4931247e2c235dd411ccdd8ba951
Chan XPS <nyeincz@Gmail.com>
2021-08-22 20:56:35 +0800

    v3

--- vote/app.py
+++ vote/app.py
@@ -5,10 +5,10 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Emacs")
-option_b = os.getenv('OPTION_B', "Vi")
+option_a = os.getenv('OPTION_A', "BTC")
+option_b = os.getenv('OPTION_B', "ETH")
 hostname = socket.gethostname()
-version = 'v1'
+version = 'v2'
 
 app = Flask(__name__)
 
